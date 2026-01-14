90ce98920901c718be9239b5154d1815a1a0d0c8
Nashrul Fikri <nashrul.fikri19@gmail.com>
2026-01-14 19:47:56 +0700

    Mengganti Kode

--- app/src/main/java/com/example/aplikasihealthcare/ui/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/aplikasihealthcare/ui/navigasi/DestinasiNavigasi.kt
@@ -1,24 +1,20 @@
 package com.example.aplikasihealthcare.ui.navigasi
 
-// Interface dasar
 interface DestinasiNavigasi {
     val route: String
     val titleRes: String
 }
 
-// 1. Halaman HOME
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = "Health Track"
 }
 
-// 2. Halaman ENTRY (Tambah Data)
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = "Catat Tensi"
 }
 
-// 3. Halaman DETAIL
 object DestinasiDetail : DestinasiNavigasi {
     override val route = "item_details"
     override val titleRes = "Detail Riwayat"
@@ -26,7 +22,6 @@ object DestinasiDetail : DestinasiNavigasi {
     val routeWithArgs = "$route/{$itemIdArg}"
 }
 
-// 4. Halaman EDIT
 object DestinasiEdit : DestinasiNavigasi {
     override val route = "item_edit"
     override val titleRes = "Edit Data"
