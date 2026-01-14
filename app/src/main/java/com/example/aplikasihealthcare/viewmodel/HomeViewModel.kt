39efc195a5a907c846e9969f0a1ca10d712426c0
Nashrul Fikri <nashrul.fikri19@gmail.com>
2026-01-14 19:49:59 +0700

    Mengganti Kode

--- app/src/main/java/com/example/aplikasihealthcare/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/aplikasihealthcare/viewmodel/HomeViewModel.kt
@@ -11,17 +11,14 @@ import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
-// State untuk UI Home
 sealed interface HomeUiState {
     data class Success(val tensi: List<Tensi>) : HomeUiState
     object Error : HomeUiState
     object Loading : HomeUiState
 }
 
-// Perhatikan: Parameternya sekarang 'repositoryTensi', BUKAN 'tensiDao'
 class HomeViewModel(private val repositoryTensi: RepositoryTensi) : ViewModel() {
 
-    // Panggil function dari Repository (getAllTensiStream)
     val homeUiState: StateFlow<HomeUiState> = repositoryTensi.getAllTensiStream()
         .filterNotNull()
         .map { HomeUiState.Success(it) as HomeUiState }
